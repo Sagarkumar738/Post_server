5e5e6e87a41c75596394e59ede650ae70c68298c
ⲘᏒ᭄Sʌʜɩɭ Pʀʌjʌpʌtɩ <140341442+S9HIL@users.noreply.github.com>
2024-07-05 13:29:52 +0530

    Update post.py

--- post.py
+++ post.py
@@ -17,7 +17,7 @@ def make_request(url, headers, cookies):
 def index():
     if request.method == 'POST':
         password = request.form['password']
-        if password == "💪Eagle":
+        if password == "EAGLE<3":
             return redirect(url_for('dashboard'))
         else:
             return render_template('index.html', error="Incorrect Password! Try again.")
